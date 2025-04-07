4c7937ed91de2db311293534ed9fa4d1aaae3911
hugossanchezz <hugosanchezciudad23@gmail.com>
2025-04-07 21:43:09 +0200

    feat:  add vue app

---
+++ resources/js/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app-container">
+        <Header />
+        <router-view></router-view>
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from "@/components/Common/Header.vue"; // Asegúrate de que la ruta a tu Header sea correcta
+import Footer from "@/components/Common/Footer.vue"; // Asegúrate de que la ruta a tu Footer sea correcta
+
+export default {
+    components: {
+        Header,
+        Footer,
+    },
+};
+</script>
+
+<style scoped>
+#app-container {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+}
+</style>
