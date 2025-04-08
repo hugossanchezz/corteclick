13f0a727e2127d3463a32a88dea458a27b6d06be
hugossanchezz <hugosanchezciudad23@gmail.com>
2025-04-08 12:59:26 +0200

    test: display actions buttons

--- resources/views/pages/LandingView.vue
+++ resources/views/pages/LandingView.vue
@@ -1,21 +1,50 @@
 <script>
+import SecondaryButton from "@/js/components/actions/SecondaryButton.vue";
+import PrimaryButton from "@/js/components/actions/PrimaryButton.vue";
+import DangerButton from "@/js/components/actions/DangerButton.vue";
+
 export default {
     name: "LoginView",
+
+    components: {
+        PrimaryButton,
+        SecondaryButton,
+        DangerButton,
+    },
+    methods: {
+        clickLogin() {
+            console.log("Ir a login");
+        },
+        clickRegister() {
+            console.log("Ir a register");
+        },
+    }
 };
 </script>
 
 <template>
     landing
+    <br />
 
     iniciar sesion | registrarse
 
+    <br />
+
+    <PrimaryButton label="Iniciar Sesión" @click="clickLogin" />
+    <PrimaryButton label="Registrarme" @click="clickRegister" />
+
+    <SecondaryButton label="Secundario"/>
+
+    <DangerButton label="Danger"/>
+
+    <br />
     svgs
-
+    <br />
     que ofrecemos
-
+    <br />
     como funcionamos
 </template>
 
-<style lang="scss">
+<style scoped lang="scss">
 
 </style>
