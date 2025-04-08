0f0ffd05cf2417b5bd48ae96340f089e93664dd9
hugossanchezz <hugosanchezciudad23@gmail.com>
2025-04-08 11:51:39 +0200

    feat:  first version components

---
+++ resources/js/components/common/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer class="main-footer">footer corteclick</footer>
+</template>
+
+<script>
+export default {
+    name: "AppFooter",
+};
+</script>
+
+<style scoped>
+/* Estilos CSS para el Footer (puedes moverlos a un archivo CSS aparte) */
+.main-footer {
+    background-color: #333;
+    color: white;
+    padding: 40px 0;
+}
+</style>
---
+++ resources/js/components/common/Header.vue
@@ -0,0 +1,43 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <router-link to="/">
+                    <!-- <img src="/images/logo.png" alt="Corteclick" /> -->
+                    LOGO
+                </router-link>
+            </div>
+
+            <nav class="main-navigation">
+                <ul>
+                    <li><router-link to="/" exact>Inicio</router-link></li>
+                    <li><router-link to="/locals">Locales</router-link></li>
+                    <li>
+                        <router-link to="/new-local"
+                            >Registrar mi local</router-link
+                        >
+                    </li>
+                </ul>
+
+                <div class="auth-buttons">
+                    <router-link to="/login" class="login-button"
+                        >Iniciar Sesión</router-link
+                    >
+                    <router-link to="/register" class="register-button"
+                        >Registrarse</router-link
+                    >
+                </div>
+            </nav>
+        </div>
+        <div class="carrusel"></div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+};
+</script>
+
+<style scoped>
+</style>
