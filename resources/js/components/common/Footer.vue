6ead6780a7af550f8d306cf791b0c3107b648a84
hugossanchezz <hugosanchezciudad23@gmail.com>
2025-04-08 12:02:23 +0200

    feat: functional components

--- resources/js/components/common/Footer.vue
+++ resources/js/components/common/Footer.vue
@@ -1,18 +1,45 @@
-<template>
-    <footer class="main-footer">footer corteclick</footer>
-</template>
-
 <script>
 export default {
     name: "AppFooter",
+
+    data() {
+        return {
+            currentYear: new Date().getFullYear(), // Obtener el año actual
+        };
+    },
 };
 </script>
+<template>
+    <footer class="main-footer">
+        <section class="copyright">
+            © {{ currentYear }} Corteclick.
+            <a
+                class="autor"
+                href="https://github.com/hugossanchezz"
+                target="_blank"
+                >Hugo Sánchez Ciudad</a
+            >.
+        </section>
+    </footer>
+</template>
 
-<style scoped>
-/* Estilos CSS para el Footer (puedes moverlos a un archivo CSS aparte) */
-.main-footer {
-    background-color: #333;
-    color: white;
-    padding: 40px 0;
+<style lang="scss">
+@use '@/sass/variables' as *;
+
+footer {
+    background-color: map-get($colores, "azul_oscuro");
+    color: map-get($colores, "blanco");
+    @include fuente("parrafo");
+
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+
+    padding: 30px 0;
+    text-align: center;
+
+    .copyright a {
+        color: map-get($colores, "naranja");
+    }
 }
 </style>
--- resources/js/components/common/Header.vue
+++ resources/js/components/common/Header.vue
@@ -1,43 +1,105 @@
+<script>
+export default {
+    name: "Header",
+
+    data() {
+        return {
+            isLandingPage: false,
+        };
+    },
+};
+</script>
+
 <template>
-    <header>
-        <div class="container">
-            <div class="logo">
-                <router-link to="/">
-                    <!-- <img src="/images/logo.png" alt="Corteclick" /> -->
-                    LOGO
-                </router-link>
+    <header class="flex-column">
+        <div class="header__navigation flex">
+            <div class="navigation__logo">
+                <router-link to="/"> Corteclik </router-link>
             </div>
 
-            <nav class="main-navigation">
-                <ul>
-                    <li><router-link to="/" exact>Inicio</router-link></li>
-                    <li><router-link to="/locals">Locales</router-link></li>
-                    <li>
+            <nav class="navigation__nav flex">
+                <div class="nav__links flex">
+                    <div class="links__item flex-center">
+                        <router-link to="/" exact>Inicio</router-link>
+                    </div>
+                    <div class="links__item flex-center">
+                        <router-link to="/appointment">Pedir cita</router-link>
+                    </div>
+                    <div class="links__item flex-center">
                         <router-link to="/new-local"
                             >Registrar mi local</router-link
                         >
-                    </li>
-                </ul>
-
-                <div class="auth-buttons">
+                    </div>
+                </div>
+            </nav>
+            <div class="navigation__auth flex">
+                <div class="auth__item flex-center">
                     <router-link to="/login" class="login-button"
                         >Iniciar Sesión</router-link
                     >
+                </div>
+                <div class="auth__item flex-center">
                     <router-link to="/register" class="register-button"
                         >Registrarse</router-link
                     >
                 </div>
-            </nav>
+            </div>
         </div>
-        <div class="carrusel"></div>
+        <div v-if="isLandingPage" class="carrusel">
+            carrusel de imagenes en la landing
+        </div>
     </header>
 </template>
 
-<script>
-export default {
-    name: "Header",
-};
-</script>
+<style scoped lang="scss">
+@use '@/sass/variables' as *;
 
-<style scoped>
+header {
+    background-color: map-get($colores, "azul_oscuro");
+    padding: 10px 50px;
+
+    a {
+        color: map-get($colores, "blanco");
+        @include fuente("titulo_secundario");
+        text-decoration: none;
+
+        &:hover {
+            color: map-get($colores, "naranja");
+        }
+    }
+
+    .navigation__logo {
+        a {
+            @include fuente("logo");
+        }
+    }
+
+    .navigation__logo a {
+        display: inline-block;
+        width: 100%;
+        height: 100%;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .navigation__nav {
+        width: 100%;
+        justify-content: space-around;
+    }
+
+    .navigation__auth {
+        width: 30%;
+    }
+
+    .links__item,
+    .auth__item {
+        padding: 10px 20px;
+    }
+
+    .carrusel {
+        color: map-get($colores, "blanco");
+    }
+}
 </style>
