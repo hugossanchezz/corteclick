3474d8c994d67ecbac04be1b92559cb7a768acab
hugossanchezz <hugosanchezciudad23@gmail.com>
2025-05-04 11:59:58 +0200

    style: new footer design

--- resources/js/components/common/Footer.vue
+++ resources/js/components/common/Footer.vue
@@ -10,21 +10,76 @@ export default {
 };
 </script>
 <template>
-    <footer class="main-footer">
-        <section class="copyright">
-            © {{ currentYear }} Corteclick.
-            <a
-                class="autor"
-                href="https://github.com/hugossanchezz"
-                target="_blank"
-                >Hugo Sánchez Ciudad</a
-            >.
+    <footer class="grid">
+        <section class="technologies flex-column">
+            <h2>Tecnologías usadas</h2>
+            <div class="logos flex">
+                <!-- <div class="flex">
+                    <img src="/img/footer/html5.svg" alt="Icono de HTML5" />HTML
+                </div>
+                <div class="flex">
+                    <img src="/img/footer/css.svg" alt="Icono de CSS" />CSS
+                </div> -->
+                <div class="flex">
+                    <img src="/img/footer/sass.svg" alt="Icono de Sass" />SASS
+                </div>
+                <!-- <div class="flex">
+                    <img
+                        src="/img/footer/javascript.svg"
+                        alt="Icono de JavaScript"
+                    />JS
+                </div> -->
+                <div class="flex">
+                    <img src="/img/footer/vue.svg" alt="Icono de Vue" />VUE
+                </div>
+                <!-- <div class="flex">
+                    <img src="/img/footer/php.svg" alt="Icono de PHP" />PHP
+                </div> -->
+                <div class="flex">
+                    <img
+                        src="/img/footer/laravel.svg"
+                        alt="Icono de Laravel"
+                    />LARAVEL
+                </div>
+            </div>
+        </section>
+        <section class="developer flex-column">
+            <h2>Desarrollador</h2>
+            <div class="logos flex">
+                <div>
+                    <a
+                        class="flex"
+                        href="https://www.linkedin.com/in/hugossanchezz/"
+                        target="_blank"
+                    >
+                        <img
+                            src="/img/footer/linkedin.svg"
+                            alt="Icono de Linkedin"
+                        />
+                        Linkedin</a
+                    >
+                </div>
+                <div>
+                    <a
+                        class="flex"
+                        href="https://github.com/hugossanchezz"
+                        target="_blank"
+                        ><img
+                            src="/img/footer/github.svg"
+                            alt="Icono de Github"
+                        />Github</a
+                    >
+                </div>
+            </div>
+        </section>
+        <section class="copyright flex-center">
+            © {{ currentYear }} Corteclick S.L. Todos los derechos reservados.
         </section>
     </footer>
 </template>
 
 <style lang="scss">
-@use '@/sass/variables' as *;
+@use "@/sass/variables" as *;
 
 footer {
     background-color: map-get($colores, "azul_oscuro");
@@ -33,11 +88,52 @@ footer {
 
     width: 100%;
 
-    padding: 30px 0;
-    text-align: center;
+    padding: 2rem 3rem;
 
-    .copyright a {
-        color: map-get($colores, "naranja");
+    grid-template:
+        "technologies developer" auto
+        "copyright copyright" auto / 1fr 1fr;
+
+    section {
+        padding: 5px;
+        align-items: center;
+
+        h2 {
+            margin-bottom: 5px;
+        }
+    }
+
+    .technologies {
+        grid-area: technologies;
+    }
+
+    .developer {
+        grid-area: developer;
+    }
+
+    .logos {
+        div {
+            padding: 5px;
+            align-items: center;
+            gap: 5px;
+            img {
+                width: 15px;
+            }
+            a {
+                color: map-get($colores, "blanco");
+                text-decoration: none;
+                gap: 5px;
+                &:hover {
+                    text-decoration: underline;
+                }
+            }
+        }
+    }
+    .copyright {
+        grid-area: copyright;
+        border-top: 1px solid map-get($colores, "gris_claro");
+        margin-top: 1rem;
+        padding: 15px 10px;
     }
 }
 </style>
